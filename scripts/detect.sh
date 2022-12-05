41141b950aeeb79d86670a00e3cc8d844f2370d9
Tomohiro Ishiguro - SE <se.ishiguro.tomohiro@gmail.com>
2022-12-05 10:02:28 +0900

    Update detect.sh

--- scripts/detect.sh
+++ scripts/detect.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+if [ ! -e "${1}/.git" ]; then
+  echo "ERROR: 指定されたフォルダは Git リポジトリではありません。"
+  exit 1
+fi
+
 docker run --rm --name detect_secrets \
   -v "${1}/src":/src:ro \
   -w /src \
